b669e81171a703f26543c211e2c814002face67c
Shamsiddin <81175691+shamsiddin-abbasov@users.noreply.github.com>
2022-12-28 18:10:51 +0500

    Add files via upload

---
+++ String/is_subsequence.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/is-subsequence/description/
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        m, n = 0, 0
+        while m < len(s) and n < len(t):
+            if s[m] == t[n]:
+                m += 1
+            n += 1
+        return m == len(s)
